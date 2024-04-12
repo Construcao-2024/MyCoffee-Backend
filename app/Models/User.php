7ec55c2f8a39ec030a721e8e3d25eb1a4e0731a6
kelvisck2 <kelvisck.cabral@ufms.br>
2024-04-11 22:21:08 -0400

    revert/v1

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+abstract class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
@@ -17,9 +17,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'id',
+        'nomeCompleto',
         'email',
-        'password',
+        'senha',
+        'fotoDePerfil'
+
     ];
 
     /**
